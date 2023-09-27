c7141d9ece802913b18318a0be0e536a7332b7a4
sufyanfauzan <sufyafauzan69@gmail.com>
2023-09-27 13:13:04 +0700

    delete all

--- app/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// memanggil file-file utama
-
-require_once 'core/App.php';
-require_once 'core/Controller.php';
-require_once 'core/Constants.php';
-
-$app = new App();
--- app/views/home/index.php
+++
@@ -1,6 +0,0 @@
-<div class="container">
-  <div class="jumbotron py-4 px-5"></div>
-  <h1 class="display-4">Selamat Datang</h1>
-  <hr class="my-4">
-  <p>Halo semua saya Sufyan, ini merupakan model dari pembelajaran saya mengenai MVC. </p>
-</div>
